88652ef94f275983949053b974d89f6eaeb27ced
Ali.Reza <alirzahosinopoor.1390@gmail.com>
2025-05-30 20:59:57 +0330

    config dirs

---
+++ src/options.py
@@ -0,0 +1,6 @@
+import os
+
+RESULTS_DIR = '../results/'
+MODELS_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'models'))
+RAW_DATA_DIR = '../data/raw/garbage-classification'
+PROCCESSED_DATA_DIR = '../data/proccessed/garbage-classification'
